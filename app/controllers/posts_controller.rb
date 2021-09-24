d4519b7a75f1a3f00bedc75102b657154708ed00
Yoshiki Kobayashi <kobayashi020202@gmail.com>
2021-09-24 13:40:58 +0900

    feat: delete appendix function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,18 +10,18 @@ class PostsController < ApplicationController
     render 'home/index'
   end
 
-  def update
-    @old_post = Post.find(params[:id])
-    if @old_post.update(post_params)
-      redirect_to post_path params[:id]
-      return
-    end
-    @post = Post.find(params[:id])
-    @new_post = Post.new
-    @new_comment = Comment.new
-    @favorites_count = @post.favorites.size
-    render 'posts/show'
-  end
+  # def update
+  #   @old_post = Post.find(params[:id])
+  #   if @old_post.update(post_params)
+  #     redirect_to post_path params[:id]
+  #     return
+  #   end
+  #   @post = Post.find(params[:id])
+  #   @new_post = Post.new
+  #   @new_comment = Comment.new
+  #   @favorites_count = @post.favorites.size
+  #   render 'posts/show'
+  # end
 
   def show
     @new_post = Post.new
@@ -30,11 +30,11 @@ class PostsController < ApplicationController
     @favorites_count = @post.favorites.size
   end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to root_path
-  end
+  # def destroy
+  #   @post = Post.find(params[:id])
+  #   @post.destroy
+  #   redirect_to root_path
+  # end
 
   private
     def post_params
