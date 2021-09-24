e9164e0f89552d141597e5bf12de22b8e0a41e89
Yoshiki Kobayashi <kobayashi020202@gmail.com>
2021-09-24 17:36:57 +0900

    feat: delete appendix comments & css to scss

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,2 @@
 class CommentsController < ApplicationController
-  # def create
-  #   @comment = Comment.new(comment_params)
-  #   if @comment.save
-  #     redirect_to post_path(params[:post_id])
-  #     return
-  #   end
-  #   @new_post = Post.new
-  #   @new_comment = Comment.new
-  #   @post = Post.find(params[:post_id])
-  #   @favorites_count = @post.favorites.size
-  #   render 'posts/show' 
-  # end
-
-  # private
-  #   def comment_params
-  #     params.require(:comment).permit(:detail).merge(post_id: params[:post_id])
-  #   end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,2 @@
 class FavoritesController < ApplicationController
-  def create
-    @favorites = Favorite.new(
-      post_id: params[:post_id]
-    )
-    @favorites.save
-    redirect_to post_path(params[:post_id])
-  end
 end
